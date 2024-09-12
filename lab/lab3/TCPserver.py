2652806a91b3cc8e3b723e9be00bd6cab3da84a8
n1ck-sm1th <m265904@usna.edu>
2024-09-12 10:32:05 -0400

    Lab

--- lab/lab3/TCPserver.py
+++ lab/lab3/TCPserver.py
@@ -1,7 +1,11 @@
 #Nicholas Smith
 #Lab03 IC322
+#AI utilized for STEP 4 as prescribed in the instruction. 
+#Specifically gave instruction on what Mimetypes was and how to
+#adequately read a file in binary mode. 
 
 from socket import *
+import mimetypes
 serverPort = 12000
 
 #Create TCP socket
@@ -12,22 +16,29 @@ serverSocket.listen(1)
 print("The server is ready to receive")
 while True:
     connectionSocket, addr = serverSocket.accept()
+    
     #This is the information from the request
     request = connectionSocket.recv(1024).decode()
-    #Print the request to the server. Part 2 step 1.
+    
     #Filename is parsed from the GET request. Method = [0] file = [1]
     filename = request.split()[1]
-    #File object is opened using the open() for reading
-    f = open(filename[1:]) 
-    #Read from the file object
+    
+    #Mimetypes figures out what type of file we are using.
+    content_type, _ = mimetypes.guess_type(filename)
+
+    #Defaults to binary stream if type cannot be determined. 
+    if content_type is None:
+        content_type = 'application/octet-stream'  
+
+    # Open the file in binary mode
+    with open(filename[1:], 'rb') as f:
+        responseBody = f.read()
+
+    # Construct the response
     responseStatus = "HTTP/1.1 200 OK\r\n"
-    responseHeader = "\r\n"
-    responseBody = " "
-    #Read from the file.
-    for line in f:
-        responseBody += line
-    response = responseStatus+responseHeader+responseBody
-    #Error testing print line.
-    #print(response)
-    connectionSocket.send(response.encode())
+    
+    #Change the content type depending on result of Mimetypes function.
+    responseHeader = f"Content-Type: {content_type}\r\nContent-Length: {len(responseBody)}\r\n\r\n"
+    response = responseStatus.encode() + responseHeader.encode() + responseBody     
+    connectionSocket.send(response)
     connectionSocket.close()
---
+++ lab/lab3/server.py
@@ -0,0 +1,55 @@
+#Nicholas Smith
+#Lab03 IC322
+#AI utilized for STEP 4 as prescribed in the instruction. 
+#Specifically gave instruction on what Mimetypes was and how to
+#adequately read a file in binary mode. 
+
+from socket import *
+import mimetypes
+import os
+
+serverPort = 12000
+
+# Create TCP socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Associate port number with socket
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    
+    # This is the information from the request
+    request = connectionSocket.recv(1024).decode()
+    
+    # Filename is parsed from the GET request. Method = [0] file = [1]
+    filename = request.split()[1]
+    
+    # Default to "404 Not Found"
+    responseStatus = "HTTP/1.1 404 Not Found\r\n"
+    responseBody = b"File not found"
+    content_type = ""
+
+    # Check if the requested file exists
+    if os.path.isfile(filename[1:]):
+        # Mimetypes figures out what type of file we are using
+        content_type, _ = mimetypes.guess_type(filename)
+        
+        # Defaults to binary stream if type cannot be determined
+        if content_type is None:
+            content_type = 'application/octet-stream'  
+
+        # Open the file in binary mode
+        with open(filename[1:], 'rb') as f:
+            responseBody = f.read()
+
+        # Construct the 200 OK response
+        responseStatus = "HTTP/1.1 200 OK\r\n"
+    
+    # Change the content type depending on the result of Mimetypes function
+    responseHeader = f"Content-Type: {content_type}\r\nContent-Length: {len(responseBody)}\r\n\r\n"
+    response = responseStatus.encode() + responseHeader.encode() + responseBody
+    
+    connectionSocket.send(response)
+    connectionSocket.close()
